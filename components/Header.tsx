67069ca275e88e54034f84d432bb255dcd4170d0
Hein Htet <heinhtetzaw0004@gmail.com>
2026-03-01 23:56:27 +0630

    chore: fix search section in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -16,15 +16,6 @@ export default function Header({ genres }: Props) {
   const [searchOpen, setSearchOpen] = useState(false);
   const router = useRouter();
 
-  useEffect(() => {
-    if (searchOpen) {
-      const el = document.getElementById(
-        "mobile-search-input",
-      ) as HTMLInputElement | null;
-      el?.focus();
-    }
-  }, [searchOpen]);
-
   function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const fd = new FormData(e.currentTarget as HTMLFormElement);
@@ -36,12 +27,14 @@ export default function Header({ genres }: Props) {
   return (
     <header className="sticky top-0 z-10 bg-accent border-b">
       <div className="flex items-center justify-between p-4">
-        {!searchOpen && (
-          <h1 className="text-2xl font-bold flex-1 flex items-center gap-1 md:flex-none">
-            <Clapperboard />
-            <span>Next Movie</span>
-          </h1>
-        )}
+        <h1
+          className={`text-lg md:text-2xl font-bold flex items-center gap-1
+          ${searchOpen ? "hidden sm:flex" : ""}
+          `}
+        >
+          <Clapperboard />
+          <span>Next Movie</span>
+        </h1>
 
         <form
           onSubmit={onSubmit}
@@ -51,7 +44,7 @@ export default function Header({ genres }: Props) {
           <Button type="submit">Search</Button>
         </form>
 
-        <div className="flex items-center sm:hidden ml-auto">
+        <div className="flex items-center sm:hidden">
           {!searchOpen ? (
             <button
               aria-label="Open search"
@@ -61,17 +54,12 @@ export default function Header({ genres }: Props) {
               <Search />
             </button>
           ) : (
-            <div className="max-w-md flex justify-between gap-5">
-              <div className="md:hidden">
-                <form onSubmit={onSubmit} className="flex gap-2">
-                  <Input
-                    id="mobile-search-input"
-                    name="q"
-                    placeholder="Search movies..."
-                  />
-                  <Button type="submit">Search</Button>
-                </form>
-              </div>
+            <div className="flex gap-5">
+              <form onSubmit={onSubmit} className="flex gap-2">
+                <Input name="q" placeholder="Search movies..." />
+                <Button type="submit">Search</Button>
+              </form>
+
               <button
                 aria-label="Close search"
                 onClick={() => setSearchOpen(false)}
